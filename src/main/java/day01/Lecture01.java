8008d9fbfdf038361d23228e26f3016bcca288da
Radwan <mdkhan.radwan@gmail.com>
2021-02-03 21:34:23 -0500

    first commit

---
+++ src/main/java/day01/Lecture01.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class Lecture01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i=0; i<=3; i++) {
+			
+			for (int j=0; j<=5; j++) {
+				
+				System.out.print(i);
+			}
+			System.out.print("*");
+		}
+		
+	}
+
+}
